b80ba7a443f01eb7344b64689affbe4f35b7b1fe
jQuery2DotNet <info@jquery2dotnet.com>
2016-06-04 10:54:01 +0530

    Sending WhatsApp Message using Dot Net CSharp

---
+++ SendingWhatsAppMessage/SendingWhatsAppMessage/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SendingWhatsAppMessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(60, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTo.Location = new System.Drawing.Point(97, 19);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(247, 26);
+            this.txtTo.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(97, 65);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(247, 123);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(97, 207);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(247, 37);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(356, 381);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Whats Message Send";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ SendingWhatsAppMessage/SendingWhatsAppMessage/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using WhatsAppApi;
+
+namespace SendingWhatsAppMessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string from = "91**********"; //(Enter your mobile number with country code)
+            string to = txtTo.Text;
+            string msg = txtMessage.Text;
+
+            // Generate WhatsApp password form this url => http://www.watools.es/pwd.html
+            WhatsApp wa = new WhatsApp(from, "WhatsAppPassword", "NickName", false, false);
+
+            // Connect Success
+            wa.OnConnectSuccess += () =>
+            {
+                MessageBox.Show("Connected to WhatsApp...");
+                // Login Success
+                wa.OnLoginSuccess += (phonenumber, data) =>
+                {
+                    wa.SendMessage(to, msg);
+                    MessageBox.Show("Message Sent...");
+                };
+                // Login Fail
+                wa.OnLoginFailed += (data) =>
+                {
+                    MessageBox.Show("Login Failed : {0} : ", data);
+                };
+                wa.Login();
+            };
+            // Connet Fail
+            wa.OnConnectFailed += (Exception) =>
+            {
+                MessageBox.Show("Connection Failed...");
+            };
+        }
+    }
+}
